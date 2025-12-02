88421c81d024402194c1f63f78584538105e5f42
Peter-Tourkoyiannis <petertourkoyiannis@gmail.com>
2025-12-01 20:51:48 -0500

    Delete query_pass.sql

--- query_pass.sql
+++
@@ -1,6 +0,0 @@
--- get invoice for member (pass)
-SELECT m.username, i.status, i.amount_due
-FROM member_pays_invoice mpi
-JOIN members m ON m.member_id = mpi.member_id
-JOIN invoice i ON i.invoice_id = mpi.invoice_id
-WHERE mpi.member_id = 1 AND mpi.invoice_id = 1;
